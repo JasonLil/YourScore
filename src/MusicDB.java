09ddbd9c82820a0a8ea83fbc6d2b46b86cbb2fa1
boydjohnson <johnson.boyd@gmail.com>
2015-05-10 08:05:02 -0500

    Adding in the database and datatable

--- src/MusicDB.java
+++ src/MusicDB.java
@@ -1,7 +1,33 @@
+import javax.swing.*;
+import java.sql.*;
+
 /**
  * Created by boyd on 5/9/15.
  */
 public class MusicDB {
+    private final String PROTOCOL = "jdbc:derby:";
+    private final String DB_NAME = "movieDB";
+    private final String USER = "user";
+    private final String PASS = "password";
+
+    private Statement statement = null;
+    private Connection conn = null;
+    private ResultSet rs = null;
+
+
+    public void setup(){
+        try{
+            conn = DriverManager.getConnection(PROTOCOL + DB_NAME + ";create=true", USER, PASS);
+
+            statement = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+
+            statement.executeUpdate("CREATE TABLE measure ()")
+
+        }catch(SQLException se){
+            System.out.println("You have already made the database.");
+        }
+    }
+
 
 
 
